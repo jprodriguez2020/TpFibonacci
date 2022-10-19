6f396d8e01e4c6742933a2471b98051f08f01578
jprodriguez2020 <jprodriguez2020@gmail.com>
2022-10-18 23:48:45 -0300

    TpFibonacci_JuanPabloRodriguez

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const app = express();
+
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const routes = require("./routes/routes.js")(app);
+
+const server = app.listen(3000, function () {
+    console.log(`Server inicializado on port: ${process.env.PORT}`);
+});
---
+++ routes/routes.js
@@ -0,0 +1,25 @@
+const appRouter = function(app) {
+	app.get("/", function(req, res) {
+		if (!req.query.number) {
+			return res.send({"status": "error", "message": "missing number"});
+		} 
+		if (req.query.number<0) {
+			return res.send({"status": "error", "message": "number should be positive"});
+		}
+		else {
+			result = fibonacci(req.query.number);
+			return res.send ({"status": "success", "result": result});
+			
+		}
+	});
+}
+
+function fibonacci(limit){
+	const fib = [0, 1];
+	for(let i = 2; i < limit; i++){
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+	return fib;
+ }
+
+module.exports = appRouter;
