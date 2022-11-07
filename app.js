a12614fe59a3604e830bf66ab532ea40a2afc6ba
jprodriguez2020 <jprodriguez2020@gmail.com>
2022-11-07 14:27:11 -0300

    Correcion .port en app.js

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 const routes = require("./routes/routes.js")(app);
 
-const server = app.listen(3000, function () {
+const server = app.listen(process.env.PORT, function () {
     console.log(`Server inicializado on port: ${process.env.PORT}`);
 });
